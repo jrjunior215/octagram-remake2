428a7ea24f17386d1bac727b7b7f4c5718fe40ab
jrjunior215 <kimop20150boon@gmail.com>
2023-08-31 11:02:57 +0700

    new creator page

--- controllers/views/creator/creatorController.js
+++ controllers/views/creator/creatorController.js
@@ -1,4 +1,11 @@
 module.exports = (req, res) => {
-  res.locals.layout = 'creator/components/layout';
-  res.render('creator/index/index', { title_nav: 'Home | Octagram' })
+  const role = SESSION_USER.role;
+
+  if (role === "USER") {
+    res.locals.layout = 'creator/components/layout';
+    res.render('creator/new_creator/index', { title_nav: 'Home | Octagram', editorContent: '' })
+  } else if (role === "CREATOR") {
+    res.redirect('/');
+  }
+
 }
