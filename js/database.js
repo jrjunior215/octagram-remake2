6b5fce3523c24751388e94b8d7adc3c541efd487
jrjunior215 <kimop20150boon@gmail.com>
2023-10-23 23:52:39 +0000

    commit from

--- js/database.js
+++ js/database.js
@@ -2,7 +2,8 @@ const mysql = require('mysql2');
 const dbConnection = mysql.createPool({
     host     : 'localhost', // MYSQL HOST NAME
     user     : 'root', // MYSQL USERNAME
-    password : '', // MYSQL PASSWORD
+    socketPath: '/var/run/mysqld/mysqld.sock',
+    password : '123qwe', // MYSQL PASSWORD
     database : 'octagram_db2' // MYSQL DB NAME
 }).promise();
 
