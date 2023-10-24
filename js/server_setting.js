50e6c444275ad9284fa6fee7196b88c1035aa919
jrjunior215 <kimop20150boon@gmail.com>
2023-10-24 14:37:40 +0700

    change localhost

--- js/server_setting.js
+++ js/server_setting.js
@@ -1,8 +1,8 @@
 // const SERVER_IP = 'localhost';
 // const SERVER_PORT = '3000';
 
-const SERVER_IP = '165.232.170.181';
-const SERVER_PORT = '3000';
+const SERVER_IP = 'localhost';
+const SERVER_PORT = '4002';
 
 module.exports = {
   SERVER_PORT,
