0fe2391b158a5666043960df4b2490118c309894
jrjunior215 <kimop20150boon@gmail.com>
2023-10-24 14:19:12 +0700

    Database chage

--- js/database.js
+++ js/database.js
@@ -6,13 +6,21 @@
 //     database : 'octagram_db2' // MYSQL DB NAME
 // }).promise();
 
+// const mysql = require('mysql2');
+// const dbConnection = mysql.createPool({
+//     host     : 'localhost', // MYSQL HOST NAME
+//     user     : 'root', // MYSQL USERNAME
+//     socketPath: '/var/run/mysqld/mysqld.sock',
+//     password : '123qwe', // MYSQL PASSWORD
+//     database : 'octagram_db2' // MYSQL DB NAME
+// }).promise();
+
 const mysql = require('mysql2');
 const dbConnection = mysql.createPool({
-    host     : 'localhost', // MYSQL HOST NAME
-    user     : 'root', // MYSQL USERNAME
-    socketPath: '/var/run/mysqld/mysqld.sock',
-    password : '123qwe', // MYSQL PASSWORD
-    database : 'octagram_db2' // MYSQL DB NAME
+    host     : '202.28.34.205', // MYSQL HOST NAME
+    user     : '63011211049', // MYSQL USERNAME
+    password : '63011211049', // MYSQL PASSWORD
+    database : 'db63011211049' // MYSQL DB NAME
 }).promise();
 
 module.exports = dbConnection;
