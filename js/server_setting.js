d9923aad9019ce13426d0ffa98937e192c8c6b50
jrjunior215 <kimop20150boon@gmail.com>
2023-10-24 14:32:26 +0700

    Server setting

--- js/server_setting.js
+++ js/server_setting.js
@@ -1,7 +1,7 @@
 // const SERVER_IP = 'localhost';
-// const SERVER_PORT = '4002';
+// const SERVER_PORT = '3000';
 
-const SERVER_IP = 'localhost';
+const SERVER_IP = '165.232.170.181';
 const SERVER_PORT = '3000';
 
 module.exports = {
